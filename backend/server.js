3c72cbdee95132e7888cda378f0d3f0cfecca555
callistusystan <callistusystan@gmail.com>
2018-08-04 23:41:19 +1000

    First draft socket server

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,58 @@
-
-import EOS from 'eosjs'
-
-var http = require('http').createServer()
+var EOS = require('eosjs');
+var http = require('http').createServer();
 var io = require('socket.io')(http);
 
+const wif = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+const pubkey = 'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV'
 
 const EOS_CONFIG = {
     contractName: 'food.ctr',
     contractSender: 'food.ctr',
     clientConfig: {
-        keyProvider: [''],
-        httpEndpoint: ''
+        keyProvider: ['5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'],
+        httpEndpoint: 'http://localhost:8888'
     }
 };
 
 const eos = EOS(EOS_CONFIG.clientConfig);
-
 var port = process.env.PORT || 3300;
 
-io.on('connecton', function(client){
-    client.on('register', handleRegister)
-    client.on('viewFood', handleViewingFood)
-    client.on('selectFood', handleSelectingFood)
+var SALES = [];
+
+io.on('connection', function(client) {
+    client.emit('getSales', sales);
 });
 
-function handleRegister(){
+io.on('getFoods', getFoods);
+io.on('getSales', getSales);
+io.on('createAcc', createAcc);
+io.on('createSale', createSale);
+io.on('processTransaction', processTransaction);
+
+function getFoods(accountName) {
+    eos.getTableRows({
+        code: EOS_CONFIG.contractName,
+        scope: accountName,
+        table:'foods',
+        json: true,
+    }).then(({ rows }) => {
+        io.emit('getFoods', rows);
+    });
+}
+
+function getSales() {
+    eos.getTableRows({
+        code: EOS_CONFIG.contractName,
+        scope: EOS_CONFIG.contractName,
+        table:'sales',
+        json: true,
+    }).then(({ rows }) => {
+        SALES = rows;
+        io.emit('getSales', rows);
+    });
+}
+
+function createAcc(accountName){
     eos.transaction(tr => {
         tr.newaccount({
             creator: 'eosio',
@@ -32,12 +60,70 @@ function handleRegister(){
             owner: pubkey,
             active: pubkey,
         })
-    })
+
+        tr.buyrambytes({
+            payer: 'eosio',
+            receiver: 'myaccount',
+            bytes: 8192
+        })
+    
+        tr.delegatebw({
+            from: 'eosio',
+            receiver: 'myaccount',
+            stake_net_quantity: '10.0000 SYS',
+            stake_cpu_quantity: '10.0000 SYS',
+            transfer: 0
+        })
+    }).then(res => {    
+        eos.contract(EOS_CONFIG.contractName).then((contract) => {
+            contract.createacc(
+                EOS_CONFIG.contractSender,
+                accountName,
+                { authorization: [EOS_CONFIG.contractSender] }
+            ).then(res => {
+                io.emit('Create New Account', { done: true });
+            })
+        })
+    });
 }
 
+function createSale({ seller, type_of_sale, qr_code, count, price, description }) {
+    eos.contract(EOS_CONFIG.contractName).then((contract) => {
+        contract.createsale(
+            {
+                seller, 
+                sale_id: SALES.length+1, 
+                type_of_sale, 
+                qr_code, 
+                count,
+                price, 
+                description
+            },
+            { authorization: [EOS_CONFIG.contractSender] }
+        ).then(res => {
+            io.emit('Create Sale', { done: true });
+        });
+    });
+}
+
+function processTransaction({ curOwner, newOwner, qr_code, count, type_of_sale, sale_id }) {
+    eos.contract(EOS_CONFIG.contractName).then((contract) => {
+        contract.setfoodowner(
+            {
+                curOwner,
+                newOwner,
+                qr_code,
+                count,
+                type_of_sale,
+                sale_id
+            },
+            { authorization: [EOS_CONFIG.contractSender] }
+        ).then(res => {
+            io.emit('Create Sale', { done: true });
+        })
+    })
+}
 
 http.listen(port, function(){
     console.log('listening on *:'+port);
 });
-
- 
