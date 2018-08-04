d269d4bfdda49872e7828b25b4520ffbda219c19
callistusystan <callistusystan@gmail.com>
2018-08-04 19:56:27 +1000

    First draft contract

--- backend/contract/food_contract.cpp
+++ backend/contract/food_contract.cpp
@@ -26,13 +26,14 @@ class foodContract : public contract {
       acc.owner = curOwner;
       acc.no_of_sales = 0;
       acc.no_of_donates = 0;
+      acc.amount = 1000;
     });
     
-    print("Added Food to ", curOwner);
+    print("Created Account: ", curOwner);
   }
   
   // @abi action
-  void createfood(account_name curOwner, uint64_t qr_code, string food_name, string expiry_date, uint32_t count) {
+  void createfood(account_name curOwner, uint64_t qr_code, string food_name, string expiry_date, uint32_t count, string units, uint32_t price) {
     require_auth(curOwner);
     // Check if curOwner has food
     foodtable curOwnerFoods(_self, curOwner);
@@ -44,13 +45,49 @@ class foodContract : public contract {
       food_record.food_name = food_name;
       food_record.expiry_date = expiry_date;
       food_record.count = count;
+      food_record.units = units;
+      food_record.price = price;
     });
     
     print("Added Food to ", curOwner);
   }
 
+
   // @abi action
-  void setfoodowner(account_name curOwner, account_name newOwner, uint64_t qr_code, uint32_t count, uint32_t type_of_sale) {
+  void createsale(account_name seller, uint32_t type_of_sale, uint64_t qr_code, uint32_t count, uint32_t price, string description) {
+    require_auth(curOwner);
+
+    // Check if curOwner has food
+    foodtable curOwnerFoods(_self, seller);
+    auto itr = curOwnerFoods.find(qr_code);
+    eosio_assert(itr != curOwnerFoods.end() && itr->count >= count, "Seller does not have enough food!");
+
+    if (type_of_sale == 1) {
+      eosio_assert(price < itr->price, "Sale price should be less than bought price!");
+    } else if (type_of_sale == 2) {
+      eosio_assert(price == 0, "Donations should be free!");
+    }
+
+    sales.emplace(seller, [&](auto & sale_item) {
+      sale_item.sale_id = (int)sales.size() + 1;
+      sale_item.type_of_sale = type_of_sale;
+      sale_item.seller = seller;
+      sale_item.qr_code = qr_code;
+      sale_item.prev_owners = itr->prev_owners;
+      sale_item.food_name = itr->food_name;
+      sale_item.expiry_date = itr->expiry_date;
+      sale_item.count = count;
+      sale_item.units = itr->units;
+      sale_item.price = price;
+      sale_item.description = description;
+    });
+    
+    print("Created Sale #", (int)sales.size() + 1);
+  }
+
+
+  // @abi action
+  void setfoodowner(account_name curOwner, account_name newOwner, uint64_t qr_code, uint32_t count, uint32_t type_of_sale, uint32_t sale_id) {
     // type = 0 means normal sale
     // type = 1 means sell to peers
     // type = 2 means donate
@@ -66,12 +103,26 @@ class foodContract : public contract {
     foodtable newOwnerFoods(_self, newOwner);
     auto itr2 = newOwnerFoods.find(qr_code);
 
+    if (type_of_sale == 1) {
+      auto itr3 = accounts.find(newOwner);
+      auto itr4 = sales.find(sale_id);
+      eosio_assert(itr3->amount >= itr4->price, "Buyer does not have enough tokens");
+
+      // sell food
+      sales.erase(itr4);
+    } else if (type_of_sale == 2) {
+      auto itr4 = sales.find(sale_id);
+      sales.erase(itr4);
+    }
+
+    // increase new owner counts
     if (itr2 != newOwnerFoods.end()) {
-      foodtable.modify(itr2, newOwner, [&](auto & food_record) {
+      newOwnerFoods.modify(itr2, newOwner, [&](auto & food_record) {
         food_record.count += count;
       });
     } else {
-      foodtable.emplace(newOwner, [&](auto & food_record) {
+
+      newOwnerFoods.emplace(newOwner, [&](auto & food_record) {
         food_record.owner = itr->newOwner;
         food_record.prev_owners = itr->prev_owners;
         food_record.prev_owners.push_back(curOwner);
@@ -82,11 +133,21 @@ class foodContract : public contract {
       });
     }
 
+    // decrease cur owner counts
+    if (itr->count == count) {
+      curOwnerFoods.erase(itr);
+    } else {
+      curOwnerFoods.modify(itr, curOwner, [&](auto & food_record){
+        food_record.count -= count;
+      });
+    }
+
     if (type_of_sale == 1) {
       auto itr = accounts.find(curOwner);
       accounts.modify(itr, curOwner, [&](auto & acc) {
         acc.no_of_sales++;
       });
+
     } else if (type_of_sale == 2) {
       auto itr = accounts.find(curOwner);
       accounts.modify(itr, curOwner, [&](auto & acc) {
@@ -105,9 +166,10 @@ class foodContract : public contract {
       account_name  owner;
       uint32_t      no_of_sales;
       uint32_t      no_of_donates;
+      uint32_t      amount;
 
       auto primary_key() const { return owner; }
-      EOSLIB_SERIALIZE(account, (owner)(no_of_sales)(no_of_donates))
+      EOSLIB_SERIALIZE(account, (owner)(no_of_sales)(no_of_donates)(amount))
     };
 
     typedef multi_index<N(accounts), account> accountstable;
@@ -122,29 +184,37 @@ class foodContract : public contract {
       string        food_name;
       string        expiry_date;
       uint32_t      count;
+      string        units;
+      uint64_t      price;
 
       auto primary_key() const { return qr_code; }
-      EOSLIB_SERIALIZE(food, (owner)(prev_owner)(qr_code)(food_name)(expiry_date)(count))
+      EOSLIB_SERIALIZE(food, (owner)(prev_owners)(qr_code)(food_name)(expiry_date)(count)(units)(price))
     };
 
     typedef multi_index<N(foods), food> foodtable;
-    foodtable foods;
 
 
 
     // @abi table sales i64
     struct sale {
-      account_name  owner;
+      uint32_t      sale_id;
+      uint32_t      type_of_sale;
+      account_name  seller;
       uint64_t      qr_code;
+      std::vector<account_name> prev_owners;
       string        food_name;
       string        expiry_date;
       uint32_t      count;
+      string        units;
+      uint32_t      price;
+      string        description;
 
-      auto primary_key() const { return qr_code; }
-      EOSLIB_SERIALIZE(food, (owner)(prev_owner)(qr_code)(food_name)(expiry_date)(count))
+
+      auto primary_key() const { return sale_id; }
+      EOSLIB_SERIALIZE(food, (sale_id)(type_of_sale)(seller)(qr_code)(prev_owners)(food_name)(expiry_date)(count)(units)(price)(description))
     };
 
-    typedef multi_index<N(sales), food> salestable;
+    typedef multi_index<N(sales), sale> salestable;
     salestable sales;
 
     
