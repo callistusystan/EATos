0809281b07dfe430e947750a626ebf4ee7d41020
David Copley <david.copley@monash.edu>
2018-08-04 17:47:43 +1000

    backup before materialui core

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import HomePage from './pages/HomePage';
+import InventoryPage from './pages/InventoryPage';
+import MarketPlacePage from './pages/MarketPlacePage';
+import ScanPage from './pages/ScanPage';
 import MobileHackathon from './components/react-mobile-hackathon';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
             <MobileHackathon appName='EOS' >
-                <HomePage />
+                <Switch>
+                    <Route path='/inventory' component={InventoryPage} />
+                    <Route path='/marketplace' component={MarketPlacePage} />
+                    <Route path='/scanpage' component={ScanPage} />
+                    <Route path='/' component={HomePage} />
+                </Switch>
+
             </MobileHackathon>
         );
     }
--- frontend/src/pages/ScanPage.js
+++ frontend/src/pages/ScanPage.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import DeviceBar from '../components/react-mobile-hackathon/devices/DeviceBar';
 import ScrollView from '../components/react-mobile-hackathon/devices/ScrollView';
 import LoadingView from '../components/react-mobile-hackathon/devices/LoadingView';
-import {GridLoader} from 'react-spinners';
+import { GridLoader } from 'react-spinners';
 import Background from '../images/background.jpg';
-
-
 
 class HomePage extends Component {
 
@@ -14,13 +12,13 @@ class HomePage extends Component {
     };
 
     componentDidMount() {
-        setTimeout(() => this.setState({ready: true}), 2000);
+        setTimeout(() => this.setState({ ready: true }), 2000);
     }
 
     renderLoading = () => {
         return (
             <LoadingView>
-                <GridLoader color='#ffb432' loading={!this.state.ready}/>
+                <GridLoader color='#ffb432' loading={!this.state.ready} />
             </LoadingView>
         );
     };
@@ -28,19 +26,19 @@ class HomePage extends Component {
     renderBody = () => {
         return (
             <ScrollView isDark>
-                <div style={{minHeight: '100%', display: 'flex', flexDirection: 'column', alignItems: "center"}}>
+                <div style={{ minHeight: '100%', display: 'flex', flexDirection: 'column', alignItems:"center" }}>
                     <div
                         className={'bluepurple'}
                         style={{
-                            width: "90%",
-                            minHeight: 100,
-                            display: "flex",
-                            borderRadius: 10,
+                            width:"90%",
+                            minHeight:100,
+                            display:"flex",
+                            borderRadius:10,
                             // marginTop:5,
                             // marginBottom:5,
-                            justifyContent: "center",
-                            alignItems: "center",
-                            color: "#fff"
+                            justifyContent:"center",
+                            alignItems:"center",
+                            color:"#fff"
                         }}
                     >
                         <h1>Scan</h1>
@@ -48,15 +46,15 @@ class HomePage extends Component {
                     <div
                         className={'bluegreen'}
                         style={{
-                            width: "90%",
-                            minHeight: 100,
-                            display: "flex",
-                            borderRadius: 10,
-                            marginTop: 5,
-                            marginBottom: 5,
-                            justifyContent: "center",
-                            alignItems: "center",
-                            color: "#fff"
+                            width:"90%",
+                            minHeight:100,
+                            display:"flex",
+                            borderRadius:10,
+                            marginTop:5,
+                            marginBottom:5,
+                            justifyContent:"center",
+                            alignItems:"center",
+                            color:"#fff"
                         }}
                     >
                         <h1>My Inventory</h1>
@@ -64,15 +62,15 @@ class HomePage extends Component {
                     <div
                         className={'orangeyellow'}
                         style={{
-                            width: "90%",
-                            minHeight: 100,
-                            display: "flex",
-                            borderRadius: 10,
-                            marginTop: 5,
-                            marginBottom: 5,
-                            justifyContent: "center",
-                            alignItems: "center",
-                            color: "#fff"
+                            width:"90%",
+                            minHeight:100,
+                            display:"flex",
+                            borderRadius:10,
+                            marginTop:5,
+                            marginBottom:5,
+                            justifyContent:"center",
+                            alignItems:"center",
+                            color:"#fff"
                         }}
                     >
                         <h1>Market Place</h1>
@@ -80,12 +78,12 @@ class HomePage extends Component {
                     <div
                         className={'whiteyellow'}
                         style={{
-                            minHeight: 100,
-                            width: "90%",
-                            display: "flex",
-                            borderRadius: 10,
-                            marginTop: 5,
-                            marginBottom: 5
+                            minHeight:100,
+                            width:"90%",
+                            display:"flex",
+                            borderRadius:10,
+                            marginTop:5,
+                            marginBottom:5
                         }}
                     >
 
@@ -99,20 +97,20 @@ class HomePage extends Component {
     render() {
         return (
             <div style={styles.container}>
-                <div
-                    style={{
-                        // height: 72,
-                        width: "100%",
-                        paddingTop: 40,
-                        display: "flex",
-                        boxShadow:"0px 0px 5px 0px #bbb",
-                        marginBottom:20
+                <DeviceBar
+                    class={'darkpurpblue'}
+                    title='Home'
+                    position='top'
+                    noBorder
+                    isAppBar
+                    titleStyle={{
+                        color: 'rgb(0,0,0)',
+                        fontFamily:"Kollektif"
                     }}
-                >
-                    <div style={{flex:1}}></div>
-                    <h1 style={{justifySelf: "center"}}>Home</h1>
-                    <div style={{flex:1}}></div>
-                </div>
+                    style={{
+                        borderColor: 'rgba(255, 255, 255, .2)'
+                    }}
+                />
                 {this.state.ready ? this.renderBody() : this.renderLoading()}
                 {/*<DeviceBar*/}
                 {/*title='Something'*/}
